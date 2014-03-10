b1510b1e637f373da0c5a905aa244b63d927a0d8
MoreFreeze <morefreeze@gmail.com>
2014-03-10 18:48:24 +0800

    generate json format result for importing mongoDB

--- bs4/lol_seek.py
+++ bs4/lol_seek.py
@@ -1,21 +1,56 @@
-''' grab lol player info from 178 API '''
 #!/bin/env python
-#coding=utf-8
+# -*- coding: utf-8 -*-
+''' grab lol player info from 178 API '''
 import urllib
 import urllib2
+import bs4
+import sys
+import re
+import json
 
 def post(url, data):
     ''' copy from web post method '''
     req = urllib2.Request(url)
-    data = urllib.urlencode(data)
+    if data != None:
+        data = urllib.urlencode(data)
     #enable cookie
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
     response = opener.open(req, data)
     return response.read()
 
-def main():
+def get(url, data):
+    ''' url get method '''
+    data = urllib.urlencode(data)
+    url = url + "?" + data
+    return post(url, None)
+
+def getPlayerMatch(server, player):
     ''' function main write jb comment '''
-    pass
+    url = 'http://lolbox.duowan.com/matchList.php'
+    data = {
+        'serverName': server,
+        'playerName': player
+    }
+    raw_content = post(url, data)
+    soup = bs4.BeautifulSoup(raw_content, from_encoding='utf-8')
+    match_arr = []
+    for t in soup.find_all(id=re.compile('cli*')):
+        t_hero = t.img['title'].encode('utf-8')
+        t_id = t['id'].encode('utf-8')
+        wins = t.find_all(class_=re.compile('green|red'))
+        if len(wins) > 0:
+            if wins[0].get_text() == u'胜利':
+                t_win = 1
+            elif wins[0].get_text() == u'失败':
+                t_win = 0
+            else:
+                t_win = -1
+        # e.g.: loadMatchDetail(6348303271,'BOT','网通四','我该拿掉谁的头颅');
+        t_mode = t['onclick'].encode('utf-8').split("'")[1]
+        t_match = {'id':t_id, 'hero':t_hero, 'win':t_win, 'mode':t_mode}
+        #print type(t_match['hero'])
+        match_arr.append(t_match)
+    print json.dumps(match_arr, ensure_ascii=False, encoding='utf-8')
 
 if __name__ == '__main__':
-    main()
+    getPlayerMatch('网通四', '我该拿掉谁的头颅')
