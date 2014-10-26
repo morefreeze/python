07a8e9c4640a737a534aa7978ecf9b1f1aa889d9
MoreFreeze <morefreeze@gmail.com>
2014-10-26 22:57:45 +0800

    add migrate

---
+++ django/WashComing/cp_app.sh
@@ -0,0 +1,9 @@
+if [ $# -lt 1 ];then
+    echo sh $0 app_name
+    exit 1
+fi
+for i in forms.py models.py serializers.py urls.py views.py; do
+    cp WCUser/$i $1/
+    sed 's/User/Cloth/g' -i $1/$i
+done
+sed 's/fo_user/fo_cloth/g' -i $1/views.py
