dbe6bfaaf12baa78b2452589f5d755c1754464e8
MoreFreeze <morefreeze@gmail.com>
2014-11-25 21:12:13 +0800

    add third category cloth

--- django/WashComing/WCCloth/urls.py
+++ django/WashComing/WCCloth/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
-from WCCloth.views import category, list, info, search
+from WCCloth.views import category, info, search
 
 urlpatterns = patterns(
     '',
     url(r'^category$', category),
-    url(r'^list$', list),
     url(r'^info$', info),
     url(r'^search$', search),
 )
