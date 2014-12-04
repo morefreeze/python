f0c6a0cb9bd6393df567bd02e993b6753320b92a
MoreFreeze <morefreeze@gmail.com>
2014-12-04 19:13:42 +0800

    fix cloth image cycle save bug

--- django/WashComing/WCCloth/models.py
+++ django/WashComing/WCCloth/models.py
@@ -24,7 +24,7 @@ class Cloth(models.Model):
     ext = JSONField(default={},blank=True)
 
     def __unicode__(self):
-        return "%s(%d %d)" % (self.get_name(), self.cid, self.price)
+        return "%s(%d %.2f)" % (self.get_name(), self.cid, self.price)
 
     def save(self, *args, **kwargs):
         # delete old file when replacing by updating the file
@@ -33,14 +33,15 @@ class Cloth(models.Model):
             s_old_image = mo_cloth.image.__str__()
             if self.image != mo_cloth.image:
                 mo_cloth.image.delete(save=False)
-            super(self.__class__, self).save(*args, **kwargs)
 # replace other model with same image
-            a_clothes = Cloth.objects.filter(image=s_old_image)
-            for it_cloth in a_clothes:
-                it_cloth.image = self.image
-                it_cloth.save()
+                if '' != s_old_image:
+                    a_clothes = Cloth.objects.filter(image=s_old_image)
+                    for it_cloth in a_clothes:
+                        it_cloth.image = self.image
+                        super(self.__class__, it_cloth).save(*args, **kwargs)
         except:
             pass # when new photo then we do nothing, normal case
+        super(self.__class__, self).save(*args, **kwargs)
 
     @classmethod
     def create(cls, d_request):
