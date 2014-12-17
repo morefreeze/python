ec3f8bbbe228ab06f0aaa2d9e6e9eb47475aa8f3
MoreFreeze <morefreeze@gmail.com>
2014-12-17 20:36:58 +0800

    change runserver to localhost

--- django/WashComing/st.sh
+++ django/WashComing/st.sh
@@ -2,11 +2,11 @@ if [ -f dj.pid ];then
     ps -p $(cat dj.pid)
     if [ $? -ne 0 ];then
         rm nohup.out 1>&2
-        nohup python manage.py runserver 0.0.0.0:8002 > dj.log &
+        nohup python manage.py runserver 127.0.0.1:8002 > dj.log &
         echo $! > dj.pid
     fi
 else
     rm nohup.out 1>&2
-    nohup python manage.py runserver 0.0.0.0:8002 > dj.log &
+    nohup python manage.py runserver 127.0.0.1:8002 > dj.log &
     echo $! > dj.pid
 fi
